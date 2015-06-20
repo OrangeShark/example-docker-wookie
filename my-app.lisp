ae98955f6ab84e3c5ee9bff50338aa1552a1502d
Erik Edrosa <erik.edrosa@gmail.com>
2015-06-20 17:16:04 -0400

    Add not found page and error handling

--- my-app.lisp
+++ my-app.lisp
@@ -9,6 +9,20 @@
 (defroute (:get "/") (req res)
   (send-response res :body "Thanks for stopping by!"))
 
+(defroute (:get ".+") (req res)
+  (send-response res :body "Page not found." :status 404))
+
+(defun my-app-error-handler (ev &optional sock)
+  (let* ((dockdata (when sock (as:socket-data sock)))
+         (response (getf sockdata :response))
+         (request (when response (response-request response))))
+    (vom:error "app: ~a" ev)
+
+    (when response
+       (wookie:send-response response :key 500 :body "error"))))
+
 (defun start ()
-  (as:with-event-loop ()
-    (start-server (make-instance 'listener :port 8000))))
+  (as:with-event-loop (:catch-app-errors 'my-app-error-handler)
+    (start-server (make-instance 'wookie:listener
+                                 :port 8000
+                                 :event-cb 'my-app-error-handler))))
